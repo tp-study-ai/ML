2819d3864f4100f4f20e5d0f026d2e491dc02e56
Лев Барабанщиков <levbara1@gmail.com>
2023-04-10 19:07:48 +0300

    First try prod

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.10-slim
+FROM pytorch/pytorch:latest
 
 COPY ./ /app
 WORKDIR /app
--- src/data/get_data.py
+++ src/data/get_data.py
@@ -1,9 +1,8 @@
-import opendatasets as od
 import os
 
+import gdown
+
 if __name__ == "__main__":
-    dataset = "https://www.kaggle.com/datasets/robertkhazhiev/codeforces-problems"
-    od.download(dataset, data_dir="data/external")
-    for file in os.listdir("data/external/codeforces-problems/"):
-        os.rename(os.path.join("data/external/codeforces-problems/", file), os.path.join("data/external/", file))
-    os.rmdir("data/external/codeforces-problems")
+    gdown.download_folder("https://drive.google.com/drive/folders/1J_DtTpxK939scBWaADvIOCdwUxS-UwNu", quiet=True,
+                          output="data/external")
+    os.system("mv data/external/MLP256_last.pth models/")
